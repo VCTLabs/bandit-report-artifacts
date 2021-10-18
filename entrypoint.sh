c63501d655b8534d7de92c887e7a87ada48f07d7
Stephen L Arnold <nerdboy@gentoo.org>
2021-10-17 21:02:24 -0700

    fix: usr: allow baseline option to exit 0 without annotations
    * avoid spurious workflow annotations when baseline is enabled
    
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,12 +15,12 @@ fi
 bandit ${BANDIT_CONFIG} -r "${INPUT_PROJECT_PATH}" -o "${GITHUB_WORKSPACE}/output/security_report.txt" -f 'txt'
 BANDIT_STATUS="$?"
 
-GITHUB_TOKEN=$INPUT_REPO_TOKEN python /main.py -r $INPUT_PROJECT_PATH
-
 if [ $BANDIT_STATUS -eq 0 ]; then
     echo "🔥🔥🔥🔥Security check passed🔥🔥🔥🔥"
     exit 0
 fi
+
+GITHUB_TOKEN=$INPUT_REPO_TOKEN python /main.py -r $INPUT_PROJECT_PATH
 
 echo "🔥🔥🔥🔥Security check failed🔥🔥🔥🔥"
 cat $GITHUB_WORKSPACE/output/security_report.txt
